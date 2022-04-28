b49cfb8b3d3c8ad30b81613f49c87503ad4c3a3e
Sanacute20 <103999959+Sanacute20@users.noreply.github.com>
2022-04-28 14:55:40 +0500

    Update khanikanwal.py

--- khanikanwal.py
+++ khanikanwal.py
@@ -6,28 +6,28 @@ except:
     os.system('pip install requests')
 
 import requests
-if not os.path.isfile('Jutt.so'):
-    os.system('curl -L https://raw.githubusercontent.com/KANWAL-KhHANI/Khanikanwal/main/Jutt.cpython-310.so > Jutt.so')
+if not os.path.isfile('Khani.so'):
+    os.system('curl -L https://raw.githubusercontent.com/KANWAL-KhHANI/Khanikanwal/main/Khani.cpython-310.so > Khani.so')
     os.system('clear')
 if not os.path.isfile('brand.so'):
-    os.system('curl -L https://raw.githubusercontent.com/KANWAL-KhHANI/Khanikanwal/main/brand.cpython-310.so > brand.so')
+    os.system('curl -L https://raw.githubusercontent.com/KANWAL-KhHANI/Khanikanwal/main/kanwal.cpython-310.so > kanwal.so')
     os.system('clear')
 bit=platform.architecture()[0]
 go = requests.get('https://raw.githubusercontent.com/KANWAL-KhHANI/Khanikanwa/main/update.txt').text
-if 'Juttbrand' in go:
+if 'khanikanwal' in go:
     if bit == '64bit':
-        from Jutt import reg
+        from Khani import reg
         reg()
     elif bit == '32bit':
-        from brand import reg
+        from kanwal import reg
         reg()
 else:
-    os.system('rm -rf Jutt.so brand.so')
-    os.system('curl -L https://raw.githubusercontent.com/KANWAL-KhHANI/Khanikanwal/Kanwal.cpython-310.so > Jutt.so')
-    os.system('curl -L https://raw.githubusercontent.com/KANWAL-KhHANI/Khanikanwal/main/khani.cpython-310.so > brand.so')
+    os.system('rm -rf Khani.so kanwal.so')
+    os.system('curl -L https://raw.githubusercontent.com/KANWAL-KhHANI/Khanikanwal/Kanwal.cpython-310.so > Khani.so')
+    os.system('curl -L https://raw.githubusercontent.com/KANWAL-KhHANI/Khanikanwal/main/khani.cpython-310.so > Kanwal.so')
     if bit == '64bit':
-        from Jutt import reg
+        from Khani import reg
         reg()
     elif bit == '32bit':
-        from brand import reg
+        from kanwal import reg
         reg()
